f86317ccc382d650dfafa847cf52241d8c07a9e2
Nico Cobelo <ingcobelo@gmail.com>
2020-12-19 17:27:12 -0300

    Create 3D cube

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -19,7 +19,30 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
+        //Create a box with dimensions
         let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
+        
+        //Create a material that is color red
+        let material = SCNMaterial()
+        
+        material.diffuse.contents = UIColor.red
+        
+        //Assign material to the cube
+        cube.materials = [material]
+        
+        //Create a point in 3d space
+        let node = SCNNode()
+         //Gave the node a position
+        node.position = SCNVector3(0, 0.1, -0.5)
+        
+        //Assign node an object to display (cube)
+        node.geometry = cube
+        
+        //Put the node in the sceneview
+        sceneView.scene.rootNode.addChildNode(node)
+        
+        //Give lighting to the box
+        sceneView.autoenablesDefaultLighting = true
         
 //        // Create a new scene
 //        let scene = SCNScene(named: "art.scnassets/ship.scn")!
